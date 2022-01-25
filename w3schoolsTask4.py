6e6c44b17ee17b821c1c3eb14199e815516fcb76
jonathanwh <jonathanwh78@gmail.com>
2022-01-25 11:21:08 +0000

    Task4

---
+++ w3schoolsTask4.py
@@ -0,0 +1,21 @@
+#Task4 w3 schools
+
+for i in range (1,10):
+    if i == 1:
+        print ("*")
+    elif i == 2:
+        print ("* *")
+    elif i == 3:
+        print ("* * *")
+    elif i == 4:
+        print ("* * * *")
+    elif i == 5:
+        print ("* * * * *")
+    elif i == 6:
+        print ("* * * *")
+    elif i == 7:
+        print ("* * *")
+    elif i == 8:
+        print ("* *")
+    elif i == 9:
+        print ("*")

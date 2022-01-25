bc56b78edd5ba7d3dbd248a77ff747ebf2d9b180
jonathanwh <jonathanwh78@gmail.com>
2022-01-25 10:25:50 +0000

    loops

---
+++ ForLoop.py
@@ -0,0 +1,8 @@
+for i in range(5, 11):
+    print(i)
+
+    #prints 5 to 10
+
+for z in range(10, 21, 2):
+    print(z)
+#prints 10 to 20 in steps of 2
---
+++ WhileLoop.py
@@ -0,0 +1,15 @@
+count = 0
+#moved tutorial code so I can get more names in the loop
+while count < 5:
+    name = str(input("What is your name? "))
+    print(name, "is awesome!")
+    count = count + 1
+
+
+#My attempt of the exercise  
+num = 0
+userentry = input("Whats your name? ")
+
+while num < 5:
+    print (userentry, "is a human")
+    num = num +1
